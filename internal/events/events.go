ffba74fed77420273f7519896e197b825f96ad6f
p0p3yee <devp0p3yee@gmail.com>
2024-02-25 23:29:57 -0500

    Update event attributes for latest fairyring

--- internal/events/events.go
+++ internal/events/events.go
@@ -7,7 +7,7 @@ import (
 
 func ProcessEvents(events map[string][]string) (uint64, string, string, error) {
 
-	attrs, found := events["keyshare-aggregated.keyshare-aggregated-block-height"]
+	attrs, found := events["keyshare-aggregated.height"]
 	if !found {
 		return 0, "", "", errors.New("aggregated keyshare event not found")
 	}
@@ -17,12 +17,12 @@ func ProcessEvents(events map[string][]string) (uint64, string, string, error) {
 		return 0, "", "", err
 	}
 
-	dataAttrs, found := events["keyshare-aggregated.keyshare-aggregated-data"]
+	dataAttrs, found := events["keyshare-aggregated.data"]
 	if !found {
 		return 0, "", "", errors.New("aggregated keyshare event data not found")
 	}
 
-	pubKeyAttr, found := events["keyshare-aggregated.keyshare-aggregated-pubkey"]
+	pubKeyAttr, found := events["keyshare-aggregated.pubkey"]
 	if !found {
 		return 0, "", "", errors.New("aggregated keyshare event pubkey not found")
 	}
